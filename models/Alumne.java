0a1438f405d14f46aa75aeac173461189daf073f
Anwar <anwarelkhattabi@iesmontsia.org>
2021-04-11 13:31:50 +0200

    Model d'alumne creat.

--- models/Alumne.java
+++ models/Alumne.java
@@ -1,4 +1,18 @@
 package Sprint5Java.models;
 
-public class Alumne {
+
+public class Alumne extends Persona{
+    private String codi;
+    public Alumne(String nom, String dni, String codi){
+        super(nom, dni);
+        this.codi = codi;
+    }
+
+    public String getCodi() {
+        return codi;
+    }
+
+    public void setCodi(String codi) {
+        this.codi = codi;
+    }
 }
